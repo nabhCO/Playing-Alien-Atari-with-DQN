203138f3a7844ec32bf62064de53728dad1e7efe
nabhCO <nabh6611@colorado.edu>
2026-08-10 04:25:17 +0530

    game and CNN are working, experiments can be run

--- alien_dqn.py
+++ alien_dqn.py
@@ -3,7 +3,7 @@ import torch
 import torchvision
 import numpy as np
 from util import transform_obs
-from parameters import gamma, alpha, epsilon, epsilon_decay, batch_size, num_episodes
+import parameters
 from CNN_DQNAgent import ConvNeuralNet, ReplayBuffer, Experience
 from gymnasium.wrappers import ResizeObservation, FrameStackObservation
 from ale_py import ALEInterface
@@ -19,7 +19,7 @@ env = FrameStackObservation(env, 4) # return stack of most recent 4 observations
 
 #initialize model, optimizer, replay buffer
 model = ConvNeuralNet()
-optimizer = torch.optim.RMSprop(model.parameters(), lr=alpha)
+optimizer = torch.optim.RMSprop(model.parameters(), lr=parameters.alpha)
 replay_buffer = ReplayBuffer(500)
 
 #function to select action based on epsilon greedy strategy, decay epsilon value
@@ -28,7 +28,7 @@ def choose_action(obs, epsilon):
     choose_random_action = np.random.choice(a=[True, False], p=[epsilon, 1-epsilon])
 
     #apply linear epsilon decay as we will take a step with the action we choose
-    new_epsilon = epsilon * epsilon_decay
+    new_epsilon = epsilon * parameters.epsilon_decay
 
     #with probability epsilon, we choose a random action to explore
     if choose_random_action:
@@ -42,7 +42,6 @@ def choose_action(obs, epsilon):
     #don't calculate gradients, we're not training the model right now
     with torch.no_grad():
 
-        obs = transform_obs(obs)
         action_vals = model.forward(obs)
         return action_vals.max(1).indices.view(1, 1), new_epsilon #returns the index for max value action
 
@@ -50,7 +49,7 @@ def choose_action(obs, epsilon):
 def training_step():
 
     #if we don't have enough experiences in the replay buffer, skip the training step
-    if replay_buffer.buffer_length() < batch_size:
+    if replay_buffer.buffer_length() < parameters.batch_size:
         return 
 
     #collect 32 random experiences from the replay buffer
@@ -77,15 +76,15 @@ def training_step():
     y_hat = torch.gather(state_vals, 1, action_training_batch)
 
     #create tensor filled with zeros
-    nextState_vals = torch.zeros(batch_size)
+    nextState_vals = torch.zeros(parameters.batch_size)
 
     #calculate actual values for non-complete nextState vals
-    with torch.no_grad:
+    with torch.no_grad():
 
         nextState_vals[non_complete_state_mask] = model.forward(nextState_training_batch).max(1).values
 
     #Q-value calculation (for complete nextStates we just take the reward, so 0 + reward for indices with complete nextState)
-    y = (nextState_vals * gamma) + reward_training_batch
+    y = (nextState_vals * parameters.gamma) + reward_training_batch
 
     #use MSE as loss function, like in the original paper (y - y_hat)^2
     loss_function = torch.nn.MSELoss()
@@ -97,9 +96,57 @@ def training_step():
     optimizer.step()
 
 
+#TRAINING
+
+'''ADD COMMENTS PLS'''
+
+for i in range(0, parameters.num_episodes):
+
+    done = False
+
+    obs, info = env.reset()
+
+    obs = transform_obs(obs)
+    
+
+    while done == False:
+
+        action, new_epsilon = choose_action(obs, parameters.epsilon)
+
+        parameters.epsilon = new_epsilon
+
+        next_obs, reward, terminated, truncated, info = env.step(action)
+        reward = torch.tensor([reward])
+
+        if terminated:
+
+            next_obs = None
+
+        else:
+
+            next_obs = transform_obs(next_obs)
+
+        replay_buffer.push(Experience(obs, action, reward, next_obs))
+
+        obs = next_obs
+
+        if obs == None:
+
+            break
+
+        training_step()
+
+        if terminated or truncated:
+
+            done == True
+
+
+print("Success")
 
 
 
+
+    
 
 
 
